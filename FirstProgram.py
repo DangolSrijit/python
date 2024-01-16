38d5b7c7487c7c820d4ce6095a292a9683ef80af
Srijit Dangol <srijit.dngl@gmail.com>
2024-01-16 22:20:35 +0545

    first python program

---
+++ FirstProgram.py
@@ -0,0 +1,11 @@
+print("hello world")
+name = input("What is your name: ")
+print("Hello "+ name)
+
+# to write multi line  text
+email = '''
+hello
+this is multi line
+thankyou.
+'''
+print(email)
